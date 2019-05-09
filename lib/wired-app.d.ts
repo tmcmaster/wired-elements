fd9d4705ded1b3046125768dac6dcf0564f47ee3
Tim McMaster <tim@mcmaster.id.au>
2019-05-09 13:15:21 +1000

    removed lib directory from the release

--- lib/wired-app.d.ts
+++
@@ -1,6 +0,0 @@
-import { WiredBase } from './wired-base';
-import './wired-pages';
-export declare class WiredApp extends WiredBase {
-    render(): any;
-    refreshElement(): void;
-}
--- lib/wired-toolbar.d.ts
+++
@@ -1,6 +0,0 @@
-import { WiredBase } from './wired-base';
-import './wired-card';
-export declare class WiredToolbar extends WiredBase {
-    render(): any;
-    refreshElement(): void;
-}
